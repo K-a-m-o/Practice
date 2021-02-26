83a8ff4669af21ab5af71932cfab9b610668cc8f
Kamohelo <576658@student.belgiumcampus.ac.za>
2021-02-26 23:32:38 +0200

    hhhh

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+
+public class Hello
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Helllo my name is Kamohelo");
+        //comment
+        //comment
+        //vv
+        System.out.println("Code for running a java programme that checks odd and even numberds ");
+        //v
+    }
+}
